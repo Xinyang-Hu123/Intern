0ed2a7d50cf88eca4e1a5576ff0e5432f0b4e1f7
sss <2755416250@qq.com>
2026-07-08 20:53:38 +0800

    fix: remove obsolete jwt interceptor registration

--- backend/sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ backend/sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -24,31 +24,6 @@ import java.util.List;
 @Slf4j
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
-    @Autowired
-    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
-
-    @Autowired
-    private JwtTokenUserInterceptor jwtTokenUserInterceptor;
-
-    /**
-     * 注册自定义拦截器
-     *
-     * @param registry
-     */
-    protected void addInterceptors(InterceptorRegistry registry) {
-        log.info("开始注册自定义拦截器...");
-
-        registry.addInterceptor(jwtTokenAdminInterceptor)
-                .addPathPatterns("/admin/**")
-                .excludePathPatterns("/admin/employee/login");
-
-        registry.addInterceptor(jwtTokenUserInterceptor)
-                .addPathPatterns("/user/**")
-                .excludePathPatterns("/user/user/login")
-                .excludePathPatterns("/user/shop/status")
-                .excludePathPatterns("/user/shop/info");
-    }
-
     /**
      * 通过knife4j生成接口文档
      * @return
